de25c4d2bf0f1f7f91b8a129d0cdf2dc731bcaeb
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-04-24 00:53:08 +0800

    Update 21条件变量实现wait和notify.cpp

--- 多线程/21条件变量实现wait和notify.cpp
+++ 多线程/21条件变量实现wait和notify.cpp
@@ -22,9 +22,11 @@ public:
 
 			unique_lock<mutex> myGuard1(my_mutex1);
 			msgRecvQueue.push_back(i);
-			//my_cond.notify_one();//我们尝试把wait线程唤醒，执行后，wait被唤醒（本身解锁）
+			//my_cond.notify_one();//我们尝试在等待这个条件变量my_cond的线程唤醒（要再去重新申请锁，并且判断后面的第二个参数是否为true）
 			//若此时没有线程卡在wait这里，则上面的notify无效果。
-			my_cond.notify_all();
+			my_cond.notify_all();//同时唤醒所有睡眠等待这个条件变量my_cond的线程,
+			//(让这些线程都唤醒，再去争夺mutex(只有一个线程能拿这个锁成功，还得卡在这不断尝试获取锁)，以及去判断第二个条件)
+			//所以此时notify_all和notify_one效果差不多(只是说在当前例子这种场景下)
 		}
 	}
 	//读共享数据函数的封装函数
@@ -67,6 +69,9 @@ public:
 			
 			/*
 			 * wait()用来等待一个东西
+			 //分为两步:
+			 //1.尝试去获取互斥量的值，尝试去上锁,若互斥量此时上锁不了，则也会睡眠等待notify唤醒。
+			 //2.若此时互斥量已上锁，则判断第二个参数的值
 			 * 如果第二个参数的lambda表达式返回值是true，并且此时互斥量Mutex处于加锁状态，那么wait()直接返回并继续执行。
 			 * 第二个参数lambad表达式返回值是false，那么wait将解锁互斥量。并阻塞到本行（睡眠），
 			 * 	阻塞到什么时候为止呢？阻塞到其他某个线程调用notify_one()成员函数为止；
@@ -74,7 +79,7 @@ public:
 				即wait()将解锁互斥量，并阻塞到本行，阻塞到其他某个线程调用notify_one()成员函数为止。
 
 			当其他线程用notify_one()将原本阻塞睡眠在wait()的线程唤醒后，恢复后的会去干啥
-			a)wait()不断尝试获取互斥量锁，如果获取不到互斥量锁,那么流程就卡在wait()这里等待获取互斥量锁(此时不会去睡眠)，
+			a)wait()不断尝试获取互斥量锁，如果获取不到互斥量锁,那么流程就卡在wait()这里等待获取互斥量锁(此时不会去睡眠(处于醒着),而是一直在这尝试去获取这个互斥量锁)，
 				如果获取到了互斥量锁，那么wait()就继续执行到b)
 			b)
 				b.1)如果wait有第二个参数(可调用对象),就判断这个lambda表达式的返回值
@@ -88,7 +93,7 @@ public:
 			
 			command = msgRecvQueue.front();
 			msgRecvQueue.pop_front();
-			sbguard1.unlock();//因为unique_lock的灵活性，可以随时解锁
+			sbguard1.unlock();//因为unique_lock的灵活性，可以随时解锁，避免锁住太长时间。
 			//下面是一些具体的动作，比如玩家抽卡等操作，，，，
 			//加入现在正在处理一个事务，需要一段时间，而不是卡在wait()等待，此时这个
 			//notify_one()函数并没有作用。也就是说notify_one()不一定可以唤醒wait（）
@@ -133,15 +138,14 @@ int main(void)
  *notify_one()--唤醒wait()进程，但是不能保证一定能唤醒
  *
  *深入思考：
+ *上面的代码可能导致出现一种情况：
  *当两个线程拿到锁的次数不一样，也就说数据处理不过来怎么办
- *		开更多的线程或者约束数据的次数
- *		实际的环境中，如果玩家的操作一直得不到执行是不可以的。
+ *因为outMsgRecvQueue()与inMsgRecvQueue()并不是一对一执行的，所以当程序循环执行很多次以后，可能在msgRecvQueue 中已经有了很多消息，
+ *但是，outMsgRecvQueue还是被唤醒一次只处理一条数据。
+ *这时可以考虑把outMsgRecvQueue多执行几次，或者对inMsgRecvQueue进行限流。
+
  *
- *很多公司喜欢要年轻人，年轻人能加班，对于简单的问题，用年轻人没问题。
- *越是困难的问题，越是要用老人，经验丰富的问题，可以把问题消失在萌芽之中。
- *重要岗位一定要使用经验丰富的人
- *
- *notify_all()函数
- *		notify_one()只能通知一个线程,要想通知多个线程就需要使用notify_one()
+ notify_one()：通知一个线程的wait()
+ notify_all()：通知所有线程的wait()
 */
 
